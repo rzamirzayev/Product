d2b80a0dd2ee89407678163f187ef0a344950b69
rzamirzayev <118839663+rzamirzayev@users.noreply.github.com>
2022-12-20 14:54:42 +0400

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,16 +1,35 @@
 using Product_task;
 using System.Runtime.CompilerServices;
 product Product = new product();
-Console.WriteLine(Product.name);
+Product.Name="Case";
+Product.Count=15;
+Product.Price = 20;
+Console.WriteLine(Product.Name);
+Console.WriteLine("1 Mehsulun qiymeti"+Product.Price+" Azndir");
+Console.WriteLine("Bu mehsuldan"+Product.Count+" eded vardir");
 
-Console.WriteLine("1 Mehsulun 1iymeti 20 Azndir");
-Console.WriteLine("Bu mehsuldan 10 eded vardir");
- 
 do
 {
-   
-        Console.WriteLine("Mehsuldan nece eded almaq istiyirsiniz");
+
+    Console.WriteLine("Mehsuldan nece eded almaq istiyirsiniz");
+    
+    
+        Product.alissayi = Convert.ToInt32(Console.ReadLine());
+    if (Product.alissayi > 0)
+        Product.Sell();
+    else if (Product.alissayi == 0)
+    {
+        Console.WriteLine("Geldiyiniz ucun tesekkurler");
+        Console.WriteLine("Borcunuz"+Product.Totallncome);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("menfi eded daxil edile bilmez yeniden daxil edin");
         Product.alissayi = Convert.ToInt32(Console.ReadLine());
         Product.Sell();
- 
-}while(Product.count>=Product.alissayi);
+    }
+    
+    
+        
+} while (Product.Count >= Product.alissayi); 
